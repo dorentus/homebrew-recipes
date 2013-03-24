2a460cf104b51039ce9c6ac7fbe3c30f4bd4544a
Zhang Yi <zhangyi.cn@gmail.com>
2013-03-24 16:27:01 +0800

    Create gw-monitor.rb

---
+++ gw-monitor.rb
@@ -0,0 +1,35 @@
+require 'formula'
+
+class GwMonitor < Formula
+  homepage 'https://github.com/dorentus/gw-monitor'
+
+  head 'https://github.com/dorentus/gw-monitor.git'
+
+  def install
+    rake "install", "prefix=#{prefix}"
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_prefix}/bin/gw-monitor</string>
+          <string>#{etc}/resolv.conf</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+      </dict>
+    </plist>
+    EOS
+  end
+end
