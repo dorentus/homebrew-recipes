3ba164a562d0f6f87b150dc06c04d59675f68a6a
Zhang Yi <zhangyi.cn@gmail.com>
2014-04-12 00:02:51 +0800

    add fomula lolcat_mruby: https://github.com/dorentus/mruby-lolcat-bin

---
+++ lolcat_mruby.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class LolcatMruby < Formula
+  homepage 'https://github.com/dorentus/mruby-lolcat-bin'
+
+  url 'https://github.com/dorentus/mruby-lolcat-bin.git', :tag => 'v0.42.1'
+  version '0.42.1'
+
+  head 'https://github.com/dorentus/mruby-lolcat-bin.git'
+
+  def install
+    rake "install", "prefix=#{prefix}"
+  end
+
+  def test
+    system "brew list | lolcat_m" 
+  end
+end
