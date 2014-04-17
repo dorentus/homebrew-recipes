31d32d0a89182c20958bfe159757926cee0048e8
Zhang Yi <zhangyi.cn@gmail.com>
2014-04-17 22:49:20 +0800

    mruby-lolcat v0.42.13, repo change

--- lolcat_mruby.rb
+++ lolcat_mruby.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class LolcatMruby < Formula
-  homepage 'https://github.com/dorentus/mruby-lolcat-bin'
+  homepage 'https://github.com/dorentus/mruby-lolcat'
 
-  url 'https://github.com/dorentus/mruby-lolcat-bin.git', :tag => 'v0.42.7'
+  url 'https://github.com/dorentus/mruby-lolcat.git', :tag => 'v0.42.13'
 
-  head 'https://github.com/dorentus/mruby-lolcat-bin.git'
+  head 'https://github.com/dorentus/mruby-lolcat.git'
 
   def install
-    system "make install PREFIX=#{prefix}"
+    system "make -C binutils install PREFIX=#{prefix}"
   end
 
   def caveats
