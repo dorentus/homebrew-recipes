263866cb09bd150f8c754d3d29ddb500c0313ada
ZHANG Yi <zhangyi.cn@gmail.com>
2019-06-25 16:23:52 +0800

    mruby-lolcat v0.42.18

--- lolcat_mruby.rb
+++ lolcat_mruby.rb
@@ -3,7 +3,7 @@ require 'formula'
 class LolcatMruby < Formula
   homepage 'https://github.com/dorentus/mruby-lolcat'
 
-  url 'https://github.com/dorentus/mruby-lolcat.git', :tag => 'v0.42.17'
+  url 'https://github.com/dorentus/mruby-lolcat.git', :tag => 'v0.42.18'
 
   head 'https://github.com/dorentus/mruby-lolcat.git'
 
